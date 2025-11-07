627e78d0ffa2acd49700d5bfd9a60c269007567a
Prav-Akula <pravs.akula@outlook.com>
2025-11-06 21:16:56 -0500

    vpc-test

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.16.0"
+    }
+
+  }
+}
+
+#backend configuration
+
+backend "s3" {
+    bucket = "remote-state-86s-bucket"
+    key    = "vpc-module"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+}
+
+#provider configuration
+
+provider "aws" {
+    region = "us-east-1"
+}
